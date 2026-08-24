0e5e6c96407c4bea27e7c89153afaa430e0c2f64
Maximiliano Garcia <maxiadrigar@gmail.com>
2026-08-23 22:38:01 -0300

    feat: establish base SEO infrastructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://8leaks-site.pages.dev",
   image: {
     remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
   },
